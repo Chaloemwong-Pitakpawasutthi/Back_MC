ab3e61ec7c98956563c4fef0e0063f0222ee7231
Chaloemwong-Pitakpawasutthi <chaloemwong.p@ku.th>
2025-11-07 16:39:51 +0700

    refactor: switch from MySQL to PostgreSQL and update dependencies

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const connectionString = process.env.DATABASE_URL || '';
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
